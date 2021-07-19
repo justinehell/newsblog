990432a76be650c26a2b2b4bdc201f7c0d38628d
Justine <hell.justine@gmail.com>
2021-07-19 10:13:42 +0200

    code refactorization

--- src/detail.js
+++ src/detail.js
@@ -1,10 +1,10 @@
 import './assets/style/global.css';
 import './assets/style/style.css';
-import Logo from './assets/images/logo.svg';
-import NewsImage from './assets/images/news.jpg';
-import dayjs from 'dayjs';
 
-let newsData = JSON.parse(sessionStorage.getItem('newsData'));
+import { getCardNewsDetail, displayLogo } from './utils/domModel';
+import { getStoredData } from './utils/sessionStorage';
+
+let newsData = getStoredData('newsData');
 let search = window.location.search; // '?id=4'
 search = search.substring(1);
 let searchParams = new URLSearchParams(search);
@@ -13,16 +13,5 @@ let currentNews = newsData[searchParams.get('id')];
 
 document.title = currentNews.title;
 
-document.getElementById('logo').src = Logo;
-
-document.querySelector('.article__tag').innerText =
-  currentNews.category.charAt(0).toUpperCase() + currentNews.category.slice(1);
-document.querySelector('.article__title').innerText = currentNews.title;
-document.querySelector('.article__date').innerText = dayjs(
-  currentNews.published_at
-).format('DD/MM/YYYY');
-document.querySelector('.article__description').innerText =
-  currentNews.description;
-document
-  .querySelector('.article__image')
-  .setAttribute('src', currentNews.image ? currentNews.image : NewsImage);
+displayLogo();
+getCardNewsDetail(currentNews);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import './assets/style/global.css';
 import './assets/style/style.css';
-import Logo from './assets/images/logo.svg';
-import NewsImage from './assets/images/news.jpg';
-import dayjs from 'dayjs';
 
-const BASE_URL = process.env.BASE_URL;
-const API_KEY = process.env.API_KEY;
-
-const LIMIT = 9;
+import { getCardNews, displayLogo } from './utils/domModel';
+import { fetchNews } from './services/newsApi';
+import { getStoredData, setStoredData } from './utils/sessionStorage';
+import { LIMIT } from './utils/appSettings';
 
 let isClicked = false;
-let isLoading = false;
 let newsOffset = 0;
 
 if (process.env.NODE_ENV !== 'production') {
@@ -19,7 +15,7 @@ if (process.env.NODE_ENV !== 'production') {
   console.log('Well hello production mode!');
 }
 
-document.getElementById('logo').src = Logo;
+displayLogo();
 
 let menuBurger = document.getElementById('toggle-menu-btn');
 let menuBottomList = document.getElementById('menu-bottom-list');
@@ -32,49 +28,10 @@ const hideMenuBottom = () => {
   menuBottomList.style.maxHeight = '0px';
 };
 
-const handleClick = (id) => {
-  let query = '?id=' + id;
-  window.location.href = 'detail.html' + query;
-};
-
-const handleMouseEnter = (id) => {
-  document.getElementById(`title_${id}`).classList.add('card__title--hover');
-  document.getElementById(`card_${id}`).classList.add('card--up');
-};
-
-const handleMouseLeave = (id) => {
-  document.getElementById(`title_${id}`).classList.remove('card__title--hover');
-  document.getElementById(`card_${id}`).classList.remove('card--up');
-};
-
-function fetchData(offset) {
-  isLoading = true;
-  let url = `${BASE_URL}access_key=${API_KEY}&languages=fr,en&limit=${LIMIT}&offset=${offset}`;
-
-  return fetch(url)
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        return Promise.reject(res);
-      }
-    })
-    .then((data) => {
-      return data.data;
-    })
-    .catch(function (err) {
-      // There was an error
-      console.warn('Something went wrong.', err);
-    })
-    .finally(() => {
-      isLoading = false;
-    });
-}
-
 const storeData = (data) => {
-  let previousNews = JSON.parse(sessionStorage.getItem('newsData')) || [];
+  let previousNews = getStoredData('newsData') || [];
   let lastestNews = [...previousNews, ...data];
-  sessionStorage.setItem('newsData', JSON.stringify(lastestNews));
+  setStoredData('newsData', lastestNews);
 };
 
 const displayData = (newsData) => {
@@ -82,44 +39,19 @@ const displayData = (newsData) => {
 
   newsData.forEach((news, i) => {
     const id = i + newsOffset;
-
-    let cardTemplateClone = document.importNode(cardTemplate.content, true);
-    let card = cardTemplateClone.querySelector('.card');
-    let cardImg = cardTemplateClone.querySelector('.card__image');
-    let cardTag = cardTemplateClone.querySelector('.card__tag');
-    let cardTitle = cardTemplateClone.querySelector('.card__title');
-    let cardDate = cardTemplateClone.querySelector('.card__date');
-
-    card.id = `card_${id}`;
-    cardTitle.id = `title_${id}`;
-    cardTitle.innerText = news.title;
-    cardTag.innerText =
-      news.category.charAt(0).toUpperCase() + news.category.slice(1);
-    cardDate.innerText = dayjs(news.published_at).format('DD/MM/YYYY');
-    cardImg.src = news.image ? news.image : NewsImage;
-
-    card.addEventListener('click', () => {
-      handleClick(id);
-    });
-    card.addEventListener('mouseenter', () => {
-      handleMouseEnter(id);
-    });
-    card.addEventListener('mouseleave', () => {
-      handleMouseLeave(id);
-    });
-    return document.querySelector('section').appendChild(cardTemplateClone);
+    getCardNews(cardTemplate, id, news);
   });
 };
 
 const showNews = async () => {
-  let data = await fetchData(newsOffset);
+  let data = await fetchNews(newsOffset);
   storeData(data);
   displayData(data);
   newsOffset += LIMIT;
 };
 
 const initialNews = () => {
-  let initialData = JSON.parse(sessionStorage.getItem('newsData')) || [];
+  let initialData = getStoredData('newsData') || [];
   displayData(initialData);
   newsOffset = initialData.length;
 };
@@ -139,4 +71,5 @@ window.addEventListener('resize', function (event) {
 });
 
 initialNews();
-sessionStorage.getItem('newsData') ? null : showNews();
+//sessionStorage.getItem('newsData') ? null : showNews();
+getStoredData('newsData') ? null : showNews();
