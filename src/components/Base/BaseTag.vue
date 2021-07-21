57acdb68d0a2f1a197f41d674a716e7928c7e46a
Justine <hell.justine@gmail.com>
2021-07-21 18:36:53 +0200

    passing data via router params Styling newsCardDetails Create base components for a news Tag, Img & Date

---
+++ src/components/Base/BaseTag.vue
@@ -0,0 +1,21 @@
+<template>
+  <span
+    class="text-blue bg-light-bg-secondary px-3 py-2 rounded text-xs font-semibold"
+    :class="{ 'text-lg': big }"
+    >{{ tag | capitalize }}</span
+  >
+</template>
+<script>
+export default {
+  name: 'BaseTag',
+  props: {
+    tag: {
+      type: String,
+      required: true,
+    },
+    big: {
+      type: Boolean,
+    },
+  },
+};
+</script>
--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -1,39 +1,40 @@
 <template>
-  <article
-    class="flex flex-col bg-light-bg rounded-2xl cursor-pointer relative bottom-0 hover:bottom-1.5 hover:shadow-xl transition-all ease-in-out duration-300"
-    @mouseenter="isHovered = true"
-    @mouseleave="isHovered = false"
+  <router-link
+    :to="{ name: 'news', params: { news: news } }"
+    style="display: inherit"
   >
-    <div class="aspect-w-4 aspect-h-3 ">
-      <img
-        class="object-cover rounded-t-2xl"
-        :src="news.image ? news.image : require('../assets/images/news.jpg')"
-        alt="News"
-      />
-    </div>
-    <div class="flex flex-grow flex-col p-6 items-start">
-      <span
-        class="text-blue bg-light-bg-secondary px-2 py-1 rounded text-xs font-semibold"
-        >{{ news.category | capitalize }}</span
-      >
-      <h2
-        class="text-dark-secondary font-semibold text-2xl px-0 py-5 transition-all ease-in-out duration-300 "
-        :class="{ 'text-blue-secondary ': isHovered }"
-      >
-        {{ news.title }}
-      </h2>
-      <div class="flex-grow"></div>
-      <span class="text-light-gray text-xs tracking-wider">{{
-        publishedDate
-      }}</span>
-    </div>
-  </article>
+    <article
+      class="flex flex-col bg-light-bg rounded-2xl cursor-pointer relative bottom-0 hover:bottom-1.5 hover:shadow-xl transition-all ease-in-out duration-300"
+      @mouseenter="isHovered = true"
+      @mouseleave="isHovered = false"
+    >
+      <BaseImage :image="news.image" />
+      <div class="flex flex-grow flex-col p-6 items-start">
+        <BaseTag :tag="news.category" />
+        <h2
+          class="text-dark-secondary font-semibold text-2xl px-0 py-5 transition-all ease-in-out duration-300 "
+          :class="{ 'text-blue-secondary ': isHovered }"
+        >
+          {{ news.title }}
+        </h2>
+        <div class="flex-grow"></div>
+        <BaseDate :date="news.published_at" />
+      </div>
+    </article>
+  </router-link>
 </template>
 
 <script>
-import dayjs from 'dayjs';
+import BaseDate from '../components/Base/BaseDate.vue';
+import BaseTag from '../components/Base/BaseTag.vue';
+import BaseImage from './Base/BaseImage.vue';
 export default {
   name: 'NewsCard',
+  components: {
+    BaseDate,
+    BaseTag,
+    BaseImage,
+  },
   props: {
     news: {
       type: Object,
@@ -44,11 +45,6 @@ export default {
     return {
       isHovered: false,
     };
-  },
-  computed: {
-    publishedDate() {
-      return dayjs(this.news.published_at).format('DD/MM/YYYY');
-    },
   },
   methods: {},
 };
